7ef7dc550280efd070eb232a36852c03aeed76ca
yang1066 <agegqqq@gmail.com>
2022-09-16 17:56:26 +0800

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,60 @@
-// import 'package:flutter/material.dart';
-// import 'package:blockie_app/pages/project_groups.dart';
-// import 'package:blockie_app/pages/projects.dart';
-// import 'package:blockie_app/pages/user.dart';
-// import 'package:blockie_app/pages/brand_detail.dart';
-// import 'package:blockie_app/pages/project_detail.dart';
-// import 'package:blockie_app/pages/image_view.dart';
-// import 'package:blockie_app/pages/nft_detail.dart';
-// import 'package:blockie_app/common/routes.dart';
-// import 'package:get/get.dart';
-//
-//
-// void main() {
-//   // final routes = {
-//   //   "/": (context) => const ProjectListView(),
-//   //   "/user": (context) => const UserPage(),
-//   //   "/brand": (context) => const BrandPage(),
-//   //   "/project": (context) => const ProjectPage(),
-//   //   '/image_view': (context) => const ImageView(),
-//   //   '/nft': (context) => NftPage()
-//   // };
-//
-//   final pages = [
-//     GetPage(
-//       name: Routes.initial,
-//       page: () => const ProjectGroups(),
-//     ),
-//     GetPage(
-//       name: Routes.projects,
-//       page: () => const Projects(),
-//     ),
-//     GetPage(
-//       name: Routes.user,
-//       page: () => const UserPage(),
-//     ),
-//     GetPage(
-//       name: Routes.brand,
-//       page: () => const BrandPage(),
-//     ),
-//     GetPage(
-//       name: Routes.project,
-//       page: () => const ProjectPage(),
-//     ),
-//     GetPage(
-//       name: Routes.imageView,
-//       page: () => const ImageView(),
-//     ),
-//     GetPage(
-//       name: Routes.nft,
-//       page: () => const NftPage(),
-//     )
-//   ];
-//
-//   runApp(GetMaterialApp(
-//     title: 'BLOCKIE',
-//     initialRoute: '/',
-//     getPages: pages,
-//     home: const ProjectGroups(),
-//   ));
-// }
-
-
 import 'package:flutter/material.dart';
+import 'package:blockie_app/pages/project_groups.dart';
+import 'package:blockie_app/pages/projects.dart';
+import 'package:blockie_app/pages/user.dart';
+import 'package:blockie_app/pages/brand_detail.dart';
+import 'package:blockie_app/pages/project_detail.dart';
+import 'package:blockie_app/pages/image_view.dart';
+import 'package:blockie_app/pages/nft_detail.dart';
+import 'package:blockie_app/common/routes.dart';
+import 'package:get/get.dart';
+
 
 void main() {
-  runApp(const MyApp());
-}
+  // final routes = {
+  //   "/": (context) => const ProjectListView(),
+  //   "/user": (context) => const UserPage(),
+  //   "/brand": (context) => const BrandPage(),
+  //   "/project": (context) => const ProjectPage(),
+  //   '/image_view': (context) => const ImageView(),
+  //   '/nft': (context) => NftPage()
+  // };
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final pages = [
+    GetPage(
+      name: Routes.initial,
+      page: () => const ProjectGroups(),
+    ),
+    GetPage(
+      name: Routes.projects,
+      page: () => const Projects(),
+    ),
+    GetPage(
+      name: Routes.user,
+      page: () => const UserPage(),
+    ),
+    GetPage(
+      name: Routes.brand,
+      page: () => const BrandPage(),
+    ),
+    GetPage(
+      name: Routes.project,
+      page: () => const ProjectPage(),
+    ),
+    GetPage(
+      name: Routes.imageView,
+      page: () => const ImageView(),
+    ),
+    GetPage(
+      name: Routes.nft,
+      page: () => const NftPage(),
+    )
+  ];
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
-  }
+  runApp(GetMaterialApp(
+    title: 'BLOCKIE',
+    initialRoute: '/',
+    getPages: pages,
+    home: const ProjectGroups(),
+  ));
 }
