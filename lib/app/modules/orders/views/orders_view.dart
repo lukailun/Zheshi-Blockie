b2d813597a071a51b3c30226f14af963429e91b7
Karen <957458041@qq.com>
2022-12-21 11:49:31 +0800

    feat: orders

--- lib/app/modules/orders/views/orders_view.dart
+++ lib/app/modules/orders/views/orders_view.dart
@@ -1,6 +1,8 @@
 import 'package:blockie_app/app/modules/orders/views/orders_item_view.dart';
 import 'package:blockie_app/data/models/app_theme_data.dart';
 import 'package:blockie_app/widgets/basic_app_bar.dart';
+import 'package:blockie_app/widgets/basic_elevated_button.dart';
+import 'package:blockie_app/widgets/blur.dart';
 import 'package:blockie_app/widgets/loading_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -17,20 +19,55 @@ class OrdersView extends GetView<OrdersController> {
         appBar: BasicAppBar(title: '我的订单'),
         body: () {
           // final orders = controller.orders.value;
-          final orders = [controller.a.value, 'b', 'c'];
+          final orders = [controller.a.value, 'b', 'c', 'd', 'dd', 'd'];
           if (orders == null) {
             return const LoadingIndicator();
           }
 
-          return ListView.separated(
-            shrinkWrap: true,
-            padding: const EdgeInsets.symmetric(horizontal: 22),
-            itemBuilder: (context, index) {
-              final order = orders[index];
-              return OrdersItemView();
-            },
-            separatorBuilder: (context, index) => const SizedBox(height: 26),
-            itemCount: orders.length,
+          return Stack(
+            children: [
+              ListView.separated(
+                shrinkWrap: true,
+                padding:
+                    const EdgeInsets.only(left: 22, right: 22, bottom: 143),
+                itemBuilder: (context, index) {
+                  final order = orders[index];
+                  return OrdersItemView();
+                },
+                separatorBuilder: (context, index) =>
+                    const SizedBox(height: 26),
+                itemCount: orders.length,
+              ),
+              Container(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  decoration: const BoxDecoration(
+                      border: Border(
+                          top: BorderSide(color: Colors.white24, width: 1))),
+                  width: double.infinity,
+                  height: 130,
+                  child: ClipRect(
+                    child: Blur(
+                      blurColor: const Color(0x10FFFFFF),
+                      colorOpacity: 0.05,
+                      child: Center(
+                        child: SizedBox(
+                          height: 48,
+                          child: BasicElevatedButton(
+                            title: '再次购买',
+                            borderRadius: 8,
+                            backgroundColor: Colors.white,
+                            textColor: Colors.black,
+                            textFontSize: 18,
+                            onTap: () {},
+                          ),
+                        ).paddingSymmetric(horizontal: 22),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           );
         }(),
       ),
